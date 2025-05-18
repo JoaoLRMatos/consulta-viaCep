7e591ca44a07f6f8ffb79bf898d17814c1b5f31e
João Luca Rodrigues <jojoaoluca02@gmail.com>
2025-05-18 20:28:58 -0300

    Add news routers /enderecos and /deletar/:id

--- src/routes/cepRoutes.js
+++ src/routes/cepRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { processarConsulta } from "../controllers/cepController.js";
+import {
+  processarConsulta,
+  listarEnderecos,
+  deletarMensagem,
+} from "../controllers/cepController.js";
 
 router.post("/consulta", processarConsulta);
+router.get("/enderecos", listarEnderecos);
+router.delete("/deletar/:id", deletarMensagem);
 
 export default router;
